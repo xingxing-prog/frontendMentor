73ce02a14db8fac22555b00f79cd71ff804a1e64
MX <dinosaurbao@pm.me>
2022-10-02 20:07:09 -0400

    verify the email

--- fylo-dark-theme-landing-page/main.js
+++ fylo-dark-theme-landing-page/main.js
@@ -1,8 +1,18 @@
 var email = document.querySelector("input");
+var validEmail = /^\S+@\S+\.\S+$/;
+var isValid = document.getElementById("left");
+
+
+
 
 email.addEventListener("input",  (e)=>{
 
-    var emailAddress = e.target.value;
-    console.log(emailAddress);
+    var emailAddress = e.target.value
+    if(emailAddress.match(validEmail) === null){
+         isValid.style.display = "block";   
+    }
+    else{
+        isValid.style.display = "none";
+    }
     
 })
