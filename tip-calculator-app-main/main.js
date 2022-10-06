695a0276e48e0acf30ed86950fbf9fcd6d76db72
MX <dinosaurbao@pm.me>
2022-10-06 12:04:11 -0400

    add some improvement on the percent selection the background and the screen display

--- tip-calculator-app-main/main.js
+++ tip-calculator-app-main/main.js
@@ -26,66 +26,127 @@ people.addEventListener("input", ()=>{
    
     people.style.color ="hsl(183, 100%, 15%)";
     numberOfPeople = parseInt(people.value);
-   
-    getTipAndTotal(numberOfPeople);
+    
+
+    if(!inputCompleted()){
+        resetTipTotal();
+    }
+    else{
+        getTipAndTotal(numberOfPeople);
+     
+    }
+    
     
      
 
 })
 
 rates.forEach((rate)=>{
+   
+    console.log(rate.classList);
     rate.addEventListener("click", (e)=>{
          
           
           //e.currentTarget.style.backgroundColor="hsl(172, 67%, 45%)";
           //e.currentTarget.style.color ="hsl(183, 100%, 15%)";
-          if(billAmount !=null && numberOfPeople !== 0){
+            
             var current = e.target;
-            current.classList.add("active");
-            tipRate = parseInt(e.target.innerHTML);
-          
+            rateBackground();
+            console.log(rate.classList);
+            current.classList.toggle("active");
+            
+           
+            tipRate = parseInt(current.value);
+    
             tip = billAmount * tipRate/100;
-        
-            getTipAndTotal(numberOfPeople);
-          }
-          
+            
+            if(!inputCompleted()){
+                resetTipTotal();
+            }
+            else{
+                getTipAndTotal(numberOfPeople);
+             
+            }
+            
           //which can make tip calculator response to any of rate
 
     })
+
 })
+
+function rateBackground(){
+    rates.forEach((rate)=>{
+        if(rate.classList.contains("active")){
+            rate.classList.toggle("active");
+        }
+    })
+}
 
 custom.addEventListener("input", (e)=>{
-    tipRate = parseInt(e.target.value);
+   var current  = e.target;
+   current.classList.toggle("active");
 
-    tip = billAmount * tipRate/100;
-    getTipAndTotal(numberOfPeople);
+            console.log(current.classList);
+            tipRate = parseInt(current.value);
+    
+            tip = billAmount * tipRate/100;
+            
+            if(!inputCompleted()){
+                resetTipTotal();
+            }
+            else{
+                getTipAndTotal(numberOfPeople);
+             
+            }
+
 })
+
+
 
 function getTipAndTotal(numberOfPeople){
 
+   
     if(numberOfPeople === 0){
         zero.style.display="block";
         people.style.outline = "2px solid red";
+       
       }
       else{
         zero.style.display ="none";
         people.style.outline = "none";
   
-        var tip = tipRate * billAmount /100;
+        tip = tipRate * billAmount /100;
         console.log(tip);
-        
+
+       
         tipEach = tip/numberOfPeople;
         
         
         tipAmount.innerHTML = (tipEach).toFixed(2);
         
         total.innerHTML = ((billAmount + tip)/numberOfPeople).toFixed(2);
+        
+      }
+}
+
+function inputCompleted(){
+    if(billAmount > 0 && tipRate > 0 && numberOfPeople >= 0){
+        return true;
+    }
+    else{
+        return false;
     }
 }
 
+function resetTipTotal(){
+    tipAmount.innerHTML = "0.00";
+    total.innerHTML = "0.00";
+}
 
-
+function initial(){
+    window.location.reload();
+}
 reset.addEventListener("click", ()=>{
-    location.reload();
+    initial();
 })
 
