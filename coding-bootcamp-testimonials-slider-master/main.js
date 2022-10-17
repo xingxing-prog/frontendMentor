5d423b5df5620e4951c41c719f8251a3621c8dc5
MX <dinosaurbao@pm.me>
2022-10-17 15:57:59 -0400

    mobile style has been finished

---
+++ coding-bootcamp-testimonials-slider-master/main.js
@@ -0,0 +1,44 @@
+var testimonials = [];
+
+var john = {
+    image: "/images/image-john.jpg",
+       
+    advice: '“ If you want to lay the best foundation possible I’d recommend taking this course.\
+    The depth the instructors go into is incredible. I now feel so confident about\
+    starting up as a professional developer."',
+    name: "John Tarkpor",
+    job: "Junior Front-end Development"
+
+};
+
+var tanya = {
+    image:"images/image-tanya.jpg",  
+    advice:'“ I’ve been interested in coding for a while but never taken the jump, until now.I couldn’t recommend this course enough. I’m now in the job of my dreams and so\
+    excited about the future. ”',
+    name: "Tanya Sinclair",
+    job: "UX Engineer"
+}
+
+testimonials.push(tanya);
+testimonials.push(john);
+
+
+
+var button = document.querySelector("button");
+var picture = document.querySelector(".person");
+var advice = document.querySelector(".advice");
+var personName = document.querySelector(".name");
+var job = document.querySelector(".job");
+
+button.addEventListener("click", (e)=>{
+    
+    var item = testimonials.pop();
+   
+    testimonials.unshift(item);
+   
+    picture.src = item.image;
+    advice.innerHTML = item.advice;
+    personName.textContent = item.name;
+    job.textContent = item.job;
+
+})
