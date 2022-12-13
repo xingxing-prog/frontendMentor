ebeae96fc2a610fd36733a29247aec5f301fbe32
MX <dinosaurbao@pm.me>
2022-12-13 18:42:14 -0500

    adjust the left

---
+++ multi-step-form-main/src/index.js
@@ -0,0 +1,3 @@
+import './style.css';
+import sidebar from "./assets/images/bg-sidebar-desktop.svg";
+
--- multi-step-form-main/webpack.config.js
+++ multi-step-form-main/webpack.config.js
@@ -1,16 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    entry: '.src/index.js',
+    entry: './src/index.js',
     output:{
         filename: "main.js",
         path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'images/[hash][ext][query]',
     },
+    
+  
 
     module:{
         rules:[{
             test: /\.css$/i,
             use: ['style-loader', 'css-loader'], 
+        },
+
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
         },
     ],
     },
